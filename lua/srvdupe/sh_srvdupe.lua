15ddccd6e6cab56984c9e835547c2e5a836c4832
Titouan Guionneau <titouan.guionneau@epita.fr>
2025-06-27 18:18:52 +0200

    Fixed typo

--- lua/srvdupe/sh_srvdupe.lua
+++ lua/srvdupe/sh_srvdupe.lua
@@ -1,5 +1,5 @@
 function SrvDupe.CheckPlyWritePermissions(ply)
     local roles = SrvDupe.Config.AllowedRolesWrite or {}
     local steamIDs = SrvDupe.Config.AllowedSteamIDWrite or {}
-    return table.HasValue(roles, ply:GetUserGroup()) or table.HasValue(steamIDs, ply:GetSteamID())
+    return table.HasValue(roles, ply:GetUserGroup()) or table.HasValue(steamIDs, ply:SteamID())
 end
